7d3fc0cf013a85fd52c19e659001382f3d1234f3
Jérémy Saint-Prix <jeremy.saintprix@gmail.com>
2023-04-25 17:35:38 +0200

    dynamic content (#13)
    * git ignore .vercel
    
    * setup prismic
    
    * fetch & render navigation
    
    * minimal header styling
    
    * render meta tags

--- app.vue
+++ app.vue
@@ -1,6 +1,47 @@
+<script setup>
+const { client } = usePrismic();
+
+const { data: index } = await useAsyncData("index", () => client.getSingle("index"), { transform: (response) => response.data });
+
+const { data: navigation } = await useAsyncData("navigation", () => client.getSingle("navigation"), { transform: (response) => response.data });
+const { logo: headerLogo, navigation_item: headerNavigation, name: headerName } = navigation.value;
+
+const open_graph = index.value.social.find((item) => item.slice_type === "open_graph").primary;
+
+useHead({
+  title: index.value.meta_title[0].text,
+  meta: [
+    {
+      property: "description",
+      content: index.value.meta_description[0].text,
+    },
+    {
+      property: "og:title",
+      content: open_graph.og_title[0].text,
+    },
+    {
+      property: "og:description",
+      content: open_graph.og_description[0].text,
+    },
+    {
+      property: "og:image",
+      content: open_graph.og_image.url,
+    },
+    {
+      property: "og:type",
+      content: "website",
+    },
+    {
+      property: "twitter:card",
+      content: "summary_large_image",
+    },
+  ],
+});
+</script>
+
 <template>
   <div>
-    <Header />
+    <Header :logo="headerLogo" :navigation="headerNavigation" :name="headerName" />
     <NFTCollection />
   </div>
 </template>
--- components/Header.vue
+++ components/Header.vue
@@ -1,16 +1,40 @@
-<template>
-  <div class="header">
-    <img :src="logo" width="500" height="82" alt="" class="logo" />
-  </div>
-</template>
-
 <script setup>
-const logo = "https://cdn.shopify.com/s/files/1/0660/8165/3002/files/GALERIE_DATA_LOGO_WHITE.png?v=1674927996&width=500";
+defineProps({
+  logo: {
+    type: Object,
+    default: () => {},
+  },
+  navigation: {
+    type: Array,
+    default: () => [],
+  },
+  name: {
+    type: Array,
+    default: () => [],
+  },
+});
 </script>
+
+<template>
+  <header class="header">
+    <PrismicImage :field="logo" class="logo" />
+
+    <nav v-if="navigation.length" class="nav">
+      <ul>
+        <li v-for="item in navigation">
+          <PrismicLink :field="item.link" :class="{ active: item.active === true }">
+            <PrismicRichText :field="item.name" />
+          </PrismicLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
 
 <style scoped>
 .header {
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   background-color: #000;
@@ -22,5 +46,21 @@ const logo = "https://cdn.shopify.com/s/files/1/0660/8165/3002/files/GALERIE_DAT
 .logo {
   max-width: 250px;
   height: auto;
+}
+
+.nav ul {
+  list-style: none;
+}
+.nav a {
+  color: #fff;
+}
+.nav a.active {
+  text-decoration: none;
+}
+
+.nav ul {
+  display: flex;
+  flex-direction: row;
+  gap: 1em;
 }
 </style>
